53ae1c674a28bc7eaf4e744da9176d439b9bd243
rangermauve <RangerMauve@hotmail.com>
2015-06-06 19:13:17 -0400

    Parse results into something less crappy

--- index.js
+++ index.js
@@ -11,6 +11,17 @@ function build_api(directory, cypher_connection) {
 
 function make_query(cypher, query_string) {
 	return function (params) {
-		return cypher.query(query_string, params);
+		return cypher.query(query_string, params).then(make_results);
 	}
+}
+
+function make_results(results) {
+	var columns = results.columns;
+	var data = results.data;
+	return data.map(function (row) {
+		return row.reduce(function (item, column, index) {
+			item[columns[index]] = column;
+			return item;
+		}, {});
+	});
 }
